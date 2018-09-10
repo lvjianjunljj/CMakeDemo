04703530d0fc9c55dae380d28b2f05bc1ba6e873
Jianjun <jianjlv@microsoft.com>
2018-09-10 14:54:19 +0800

    first commit

---
+++ CMakeDemo/CMakeDemo.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeDemo.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeDemo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	system("pause");
+	return 0;
+}
